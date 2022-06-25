628d36ae453d616df2d85d4f5997018e0309fbbc
youngsun <dudtjs1021@hansung.ac.kr>
2022-06-26 01:30:05 +0900

    고양이 상세 뷰 구현

--- GDGTeam8/GDGTeam8/CatDetail/View/CatDetailViewController.swift
+++ GDGTeam8/GDGTeam8/CatDetail/View/CatDetailViewController.swift
@@ -14,6 +14,8 @@ class CatDetailViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     feedCollectionView.register(UINib(nibName: "CatFeedCell", bundle: .main), forCellWithReuseIdentifier: "CatFeedCell")
+    feedCollectionView.register(UINib(nibName: "CatInfoCell", bundle: .main), forCellWithReuseIdentifier: "CatInfoCell")
+    feedCollectionView.register(UINib(nibName: "CatFeedPlusCell", bundle: .main), forCellWithReuseIdentifier: "CatFeedPlusCell")
     feedCollectionView.delegate = self
     feedCollectionView.dataSource = self
 
@@ -22,25 +24,69 @@ class CatDetailViewController: UIViewController {
 
 
 extension CatDetailViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 2
+  }
+  
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    if section == 0 {
+      return 1
+    }
+    else {
+      return 10
+    }
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = feedCollectionView.dequeueReusableCell(withReuseIdentifier: "CatFeedCell", for: indexPath) as? CatFeedCell else { return UICollectionViewCell() }
-    return cell
+    if indexPath.section == 0 {
+      guard let cell = feedCollectionView.dequeueReusableCell(withReuseIdentifier: "CatInfoCell", for: indexPath) as? CatInfoCell else { return UICollectionViewCell() }
+      return cell
+    }
+    else {
+      if indexPath.row == 0 {
+        guard let cell = feedCollectionView.dequeueReusableCell(withReuseIdentifier: "CatFeedPlusCell", for: indexPath) as? CatFeedPlusCell else { return UICollectionViewCell() }
+        return cell
+      }
+      
+      else {
+        guard let cell = feedCollectionView.dequeueReusableCell(withReuseIdentifier: "CatFeedCell", for: indexPath) as? CatFeedCell else { return UICollectionViewCell() }
+        return cell
+        
+      }
+      
+    }
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-    return 10
+    if section == 0 {
+      return 0
+    }
+    else {
+      return 18
+    }
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-    return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    if section == 0 {
+      return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    else {
+      return UIEdgeInsets(top: 10, left: 15, bottom: 10, right: 15)
+    }
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return CGSize(width: collectionView.frame.width - 20, height: 20)
+    if indexPath.section == 0 {
+      return CGSize(width: collectionView.frame.width, height: 490)
+    }
+    
+    else {
+      return CGSize(width: collectionView.frame.width/2 - 20, height: collectionView.frame.width/2 - 15)
+    }
+    
   }
   
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
--- GDGTeam8/GDGTeam8/CatDetail/View/Cell/CatFeedPlusCell.swift
+++ GDGTeam8/GDGTeam8/CatDetail/View/Cell/CatFeedPlusCell.swift
@@ -1,13 +1,13 @@
 //
-//  CatFeedCell.swift
+//  CatFeedPlusCell.swift
 //  GDGTeam8
 //
-//  Created by 임영선 on 2022/06/25.
+//  Created by 임영선 on 2022/06/26.
 //
 
 import UIKit
 
-class CatFeedCell: UICollectionViewCell {
+class CatFeedPlusCell: UICollectionViewCell {
 
   @IBOutlet weak var backView: UIView!
   override func awakeFromNib() {
