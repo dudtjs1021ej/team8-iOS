7a8bcf7af7613f97628ed3e1c832a4ff1c3c49af
백상휘 <sanghwiback@gmail.com>
2022-06-25 17:59:42 +0900

    feature: add map to track user

--- GDGTeam8/GDGTeam8/CatMain/CatMainViewController.swift
+++ GDGTeam8/GDGTeam8/CatMain/CatMainViewController.swift
@@ -18,8 +18,33 @@ class CatMainViewController: UIViewController {
     
     @IBOutlet weak var switchFeedButton: UIButton!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mainMapView.showsUserLocation = true
+    }
+    
+    func goLocation(_ location: Location, delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(location.latitude, location.longitude)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        
+        mainMapView.setRegion(pRegion, animated: true)
+        
+        return pLocation
+    }
+    
+    func setPin(_ location: Location, delta span: Double, title: String, subTitle: String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = goLocation(location, delta: span)
+        annotation.title = title
+        annotation.subtitle = subTitle
+        mainMapView.addAnnotation(annotation)
     }
     
     @IBAction func switchMapButtonTouchUpInside(_ sender: UIButton) {
@@ -29,5 +54,33 @@ class CatMainViewController: UIViewController {
     }
     
     @IBAction func switchFeedButtonTouchUpInside(_ sender: UIButton) {
+    }
+}
+
+extension CatMainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let pLocation = locations.last else { return }
+        _ = goLocation(Location(longitude: pLocation.coordinate.longitude, latitude: pLocation.coordinate.latitude), delta: 0.01)
+        CLGeocoder().reverseGeocodeLocation(pLocation) { placeMarks, error in
+            let pm = placeMarks?.first
+            
+            var address = ""
+            
+            if let country = pm?.country {
+                address = country
+            }
+            
+            if let pmLocality = pm?.locality {
+                address += " \(pmLocality)"
+            }
+            
+            if let pmThoroughFare = pm?.thoroughfare {
+                address += " \(pmThoroughFare)"
+            }
+            
+            print(address)
+        }
+        locationManager
+            .stopUpdatingLocation()
     }
 }
