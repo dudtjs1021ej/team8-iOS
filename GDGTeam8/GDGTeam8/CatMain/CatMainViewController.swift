533677cf42bcad22a44387bb02c60f2f20490761
백상휘 <sanghwiback@gmail.com>
2022-06-25 16:52:24 +0900

    chore: create cat main view

---
+++ GDGTeam8/GDGTeam8/CatMain/CatMainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CatMainViewController.swift
+//  GDGTeam8
+//
+//  Created by 백상휘 on 2022/06/25.
+//
+
+import UIKit
+import MapKit
+
+class CatMainViewController: UIViewController {
+
+    @IBOutlet weak var mainMapView: MKMapView!
+    
+    @IBOutlet weak var switchMapButton: UIButton!
+    
+    @IBOutlet weak var createFeedButton: UIButton!
+    
+    @IBOutlet weak var switchFeedButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func switchMapButtonTouchUpInside(_ sender: UIButton) {
+    }
+    
+    @IBAction func createFeedButtonTouchUpInside(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchFeedButtonTouchUpInside(_ sender: UIButton) {
+    }
+}
